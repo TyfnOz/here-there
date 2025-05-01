8df8e97e61fe8cc2b6063e2bf978cb7e24c7413f
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2025-05-01 23:57:39 +0300

    create property page

---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,30 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Prisma } from "@prisma/client";
+
+//const name = Prisma.PropertyScalarFieldEnum.price
+
+type PriceInputProps = {
+    defaultValue?: number;
+}
+
+function PriceInput({defaultValue}:PriceInputProps) {
+  const name = 'price';
+  return (
+    <div className="mb-2">
+        <Label htmlFor={name} className="capitalize mb-1">
+            Price ($)
+        </Label>
+        <Input 
+            id={name} 
+            type="number" 
+            name={name} 
+            min={0} 
+            defaultValue={defaultValue || 100} 
+            required
+        />
+    </div>
+  )
+}
+
+export default PriceInput
