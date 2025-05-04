55dea5d3a164e38a77cd966cceb7a2d559e48442
Tayfun Özdemir <tyfnzdmr0@gmail.com>
2025-05-05 02:51:39 +0300

    api format fix

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,10 +1,10 @@
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-import { type NextRequest, type NextResponse } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import db from '@/utils/db';
 import { formatDate } from '@/utils/format';
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
   const requestHeaders = new Headers(req.headers);
   const origin = requestHeaders.get('origin');
   const { bookingId } = await req.json();
@@ -21,7 +21,7 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     },
   });
   if (!booking) {
-    return Response.json(null, {
+    return NextResponse.json(null, {
       status: 404,
       statusText: 'Not Found',
     });
@@ -57,10 +57,10 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
       mode: 'payment',
       return_url: `${origin}/api/confirm?session_id={CHECKOUT_SESSION_ID}`,
     });
-    return Response.json({ clientSecret: session.client_secret });
+    return NextResponse.json({ clientSecret: session.client_secret });
   } catch (error) {
     console.log(error);
-    return Response.json(null, {
+    return NextResponse.json(null, {
       status: 500,
       statusText: 'Internal Server Error',
     });
